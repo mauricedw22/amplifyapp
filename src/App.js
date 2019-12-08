a20472e2b44604b5ecfe0abfee77f928130e63b6
maurice <mauricedw22@gmail.com>
2019-12-07 19:56:23 -0500

    added YourPerfectWeb

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>, add <a href="http://yourperfectweb.com">Your Perfect Web</a> and save to reload.
         </p>
         <a
           className="App-link"
